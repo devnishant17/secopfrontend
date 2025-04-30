7692225ac082eda8f11f392f6fac05090347bace
Nishant kumar <kumarnishant760@gmail.com>
2025-04-30 18:38:53 +0530

    ddf1

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,6 +24,7 @@ const HomePage = () => {
                 <div className="wrapper home-default-wrapper">
                     <Header />
                 <div className="main-content site-wrapper-reveal">
+                <IntroContainer />
                 <Counter />
                 <IntroContainer />
                       <Counter />
